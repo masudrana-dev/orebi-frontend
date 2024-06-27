23aa4ea26ca921c0e69aa1cb59037ce0578f2d32
Masud Rana <masud.cphd@gmail.com>
2024-06-28 00:38:03 +0600

    react-slick-slider

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,14 +2,60 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import banner from '../../assets/banner.png'
+import { useState } from "react";
 
 const Banner = () => {
+    const [activeDot, setActiveDot] = useState(' ');
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        beforeChange: (next) => {
+            setActiveDot({ next });
+        },
+        appendDots: (dots) => (
+            <div
+                style={{
+                    position: "absolute",
+                    top: "40%",
+                    left: "150px"
+                }}
+            >
+                <ul style={{ margin: "0px" }}>
+                    {dots.map((item) => {
+                        return <li style={{ display: "block", marginTop: "10px" }}> {item}</li>;
+                    })}
+                </ul>
+            </div >
+        ),
+        customPaging: i => (
+            <div
+                style={
+                    i === activeDot
+                        ? {
+                            width: "30px",
+                            borderRight: "3px black solid",
+                            padding: "10px 30px 10px 0px",
+                            fontFamily: "dm sans",
+                            fontSize: '14px',
+                            color: "#262626"
+                        }
+                        :
+                        {
+                            width: "30px",
+                            borderRight: "3px white solid",
+                            padding: "10px 30px 10px 0px",
+                            fontFamily: "dm sans",
+                            fontSize: '14px',
+                            color: 'black'
+                        }
+                }
+            >
+                0{i + 1}
+            </div >
+        )
     };
     return (
         <div className="slider-container">
